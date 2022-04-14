21779b0034d91d570bbbbf508188d3541afe11aa
jmsmg <jmsmg1@me.com>
2022-04-14 20:28:48 +0900

    url, view 지정->base.html 만들어야함

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+def blog_index(request):
+    post = Post.objects.all().order_by('-created_at')
+    context = {'post': post}
+    return render(request, 'blog/post_index.html', context)
+    
