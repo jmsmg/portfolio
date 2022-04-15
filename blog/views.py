32bfe1762200bb9b914204b20a24e4018b54b164
jmsmg <jmsmg1@me.com>
2022-04-15 03:07:30 +0000

    클래스명 오류수정

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Post
 # Create your views here.
 
-def blog_index(request):
+def post_index(request):
     post = Post.objects.all().order_by('-created_at')
     context = {'post': post}
     return render(request, 'blog/post_index.html', context)
