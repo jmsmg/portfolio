c7a6f340e8cf9099ae830b3632e060180bead3d6
jmsmg <jmsmg1@me.com>
2022-04-19 02:31:12 +0900

    hello 구현

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,7 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_ㅔview(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from lotto import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('', views.index),
+    path('hello/', views.hello, name='hello_main'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- lotto/views.py
+++ lotto/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('<h1>Hello Worlds</h1>')
+    # return render('templates/index.html', {
+
+    # })
+
+def hello(request):
+    return HttpResponse('<h1 style="color:red">Hello Worlds</h1>')
