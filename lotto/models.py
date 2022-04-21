df5d5701f8e030127b91e565c6c0f88e8de794f6
jmsmg <jmsmg1@me.com>
2022-04-22 05:47:13 +0900

    제너레이터

--- lotto/models.py
+++ lotto/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import random
+
+from pytz import timezone
 
 # Create your models here.
 
@@ -8,3 +11,18 @@ class GuessNumbers(models.Model):
     lottos = models.CharField(max_length=255, default='[1, 2, 3, 4, 5]')
     num_lotto = models.IntegerField(default=5)
     update_date = models.DateTimeField()
+
+    def generate(self):
+        self.lottos = ""
+        origin = list(range(1,46))
+
+        for i in range(0, self.num_lotto):
+            random.shuffle(origin)
+            guess = origin[:6]
+            guess.sort()
+            self.lottos += str(guess) + '\n' # 로또번호를 str에 6개 번호 set 추가
+        self.update_date = timezone.now()
+        self.save() # GuessNumbers object를 DB에 저장
+
+    def __str__(self) -> str:
+        return f'pk {self.pk} : {self.name} - {self.text}'
