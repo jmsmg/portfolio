0591a3ef2e5dafb721fe6b761fbf451c048c13a9
jmsmg <jmsmg1@me.com>
2022-04-21 21:13:26 +0900

    로또 db 제너레이트 써야함

--- lotto/models.py
+++ lotto/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class GuessNumbers(models.Model):
+    name = models.CharField(max_length=24)
+    text = models.CharField(max_length=255)
+    lottos = models.CharField(max_length=255, default='[1, 2, 3, 4, 5]')
+    num_lotto = models.IntegerField(default=5)
+    update_date = models.DateTimeField()
