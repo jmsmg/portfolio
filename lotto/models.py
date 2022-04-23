f6e54c3093ff53310d9cc1068ec32f9aa9fabf84
jmsmg <jmsmg1@me.com>
2022-04-23 23:26:42 +0900

    테스트 케이스

--- lotto/models.py
+++ lotto/models.py
@@ -1,14 +1,14 @@
+from django.utils import timezone
 from django.db import models
 import random
 
-from pytz import timezone
 
 # Create your models here.
 
 class GuessNumbers(models.Model):
     name = models.CharField(max_length=24)
     text = models.CharField(max_length=255)
-    lottos = models.CharField(max_length=255, default='[1, 2, 3, 4, 5]')
+    lottos = models.CharField(max_length=255, default='[1, 2, 3, 4, 5, 6]')
     num_lotto = models.IntegerField(default=5)
     update_date = models.DateTimeField()
 
@@ -24,5 +24,5 @@ class GuessNumbers(models.Model):
         self.update_date = timezone.now()
         self.save() # GuessNumbers object를 DB에 저장
 
-    def __str__(self) -> str:
-        return f'pk {self.pk} : {self.name} - {self.text}'
+    # def __str__(self) -> str:
+    #     return f'pk {self.pk} : {self.name} - {self.text}'
