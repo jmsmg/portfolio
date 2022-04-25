2ad66cbe53086317d84443fe477ba0c6c35c8eff
jmsmg <jmsmg1@me.com>
2022-04-25 12:56:57 +0900

    django form 생성

--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     # path('', views.index),
     path('hello/', views.hello, name='hello_main'),
     path('lotto/', views.index, name='index'),
+    path('lotto/new', views.post, name='new_lotto'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- lotto/views.py
+++ lotto/views.py
@@ -1,14 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import GuessNumbers
+from .forms import PostForm
 
 # Create your views here.
 
 def index(request):
-    # return HttpResponse('<h1>Hello Worlds</h1>')
+    lottos = GuessNumbers.objects.all()
+    sample_sentence = '안녕하세요'
     return render(request, 'lotto/default.html', {
+        'lottos':lottos, # context
+        })
 
-    })
 
 def hello(request):
     return HttpResponse('<h1 style="color:red">Hello Worlds</h1>')
-  
+
+
+def post(request):
+    form = PostForm()
+    return render(request, 'lotto/form.html', {
+        'form':form,
+        })
